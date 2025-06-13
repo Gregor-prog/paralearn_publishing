1dda25d28d706c134fa11ae166331b606622138b
Greg-prog <olajideemmanuel250@gmail.com>
2025-06-13 14:46:00 +0100

    brevo for foorer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,10 +1,41 @@
 import { FaFacebookF, FaTwitter, FaInstagram, FaLinkedin } from 'react-icons/fa';
 import logo from "../assets/logo_1.jpg"
 import { Link } from 'react-router-dom';
-
+import { Subscribe } from "../helper/newsLetterSub";
+import { sendEmail } from "../helper/sendEmail";
+import { useState } from 'react';
+import { Toaster,toast } from 'sonner';
 function Footer() {
+
+      const [emailState, setEmailState] = useState("")
+      async function newsLetter(e){
+          e.preventDefault()
+          const formData = new FormData(e.target)
+          const email = formData.get('email')
+          console.log(email)
+          setEmailState("")
+          try {
+              
+          const responseSubscribe = await Subscribe(email)
+          const responseEmail = await sendEmail(email)
+  
+          if(responseEmail.ok && responseSubscribe.ok){
+              toast.success("You have registered for our newsLetter, an email will be sent shortly")
+          }
+  
+          } catch (error) {
+              if(error){
+                  console.log(error)
+                  console.log('an error occured')
+                  toast.error("an error occured, try again later")
+              }
+          }
+      }
+
+
   return (
     <footer className="bg-[#0a0a0a] text-white py-10 px-6 md:px-20">
+    <Toaster richColors position='top-right'/>
       <div className="grid grid-cols-1 md:grid-cols-4 gap-10">
         
         {/* Logo & Mission */}
@@ -43,14 +74,17 @@ function Footer() {
         <div>
           <h3 className="text-lg font-semibold mb-3">Join Our Community</h3>
           <p className="text-sm mb-2 text-gray-400">Subscribe for writing tips, events, and publishing resources.</p>
-          <div className="flex overflow-hidden rounded-md mb-4">
-            <input
+          
+            <form action="" onSubmit={(e) => {newsLetter(e)}} className='flex overflow-hidden rounded-md mb-4'>
+              <input
               type="email"
               placeholder="you@example.com"
-              className="w-[50px] px-3 py-2 text-[black] bg-[#313131]"
+              className="w-[70px] px-3 py-2 text-[black] bg-[#313131]"
+              onChange={(e) => setEmailState(e.target.value)} value={emailState}
+              name='email'
             />
             <button className="bg-[#ff4d4f] text-white px-4">Subscribe</button>
-          </div>
+            </form>
           <div className="flex gap-4 text-[#ff4d4f] text-xl">
             <a href="https://www.facebook.com/dparakletus/"><FaFacebookF /></a>
             <a href="https://twitter.com/parakletus_ng"><FaTwitter /></a>
